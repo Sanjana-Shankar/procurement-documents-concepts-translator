e2eff1dd503e6fcba9758a0d3d85b89a70e2c1fa
yukkigu <yukkigu@gmail.com>
2025-10-03 14:49:59 -0700

    UI improvements to landing page

--- frontend/src/Components/UploadModal.jsx
+++ frontend/src/Components/UploadModal.jsx
@@ -3,7 +3,8 @@ import React, {useState, useRef} from "react";
 import "./UploadModal.css"
 
 function UploadModal({ isOpen, onClose, onFileSelect }) {
-  // const [isDragging, setIsDragging] = useState(false);
+  const [isDragging, setIsDragging] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const fileInputRef = useRef(null);
 
   if (!isOpen) return null; // don’t render unless open
@@ -11,7 +12,13 @@ function UploadModal({ isOpen, onClose, onFileSelect }) {
   const handleFileChange = (event) => {
     const file = event.target.files[0];
     if (file) {
-      onFileSelect(file);
+      // show loading spinner
+      setIsLoading(true);
+      // simulate parse
+      setTimeout(() => {
+        onFileSelect(file);
+        setIsLoading(false);
+      }, 2000)
     }
   };
 
@@ -32,22 +39,29 @@ function UploadModal({ isOpen, onClose, onFileSelect }) {
         className="modal-container" 
         onClick={(e) => e.stopPropagation()}
       >
-        <h2 className="upload-title">Upload a File</h2>
-        <div className="drag-area">
-          <p>Drag and Drop file here</p>
-          <p>Or</p>
-          <button className="browse-button" onClick={triggerFileInput}>
-            Browse File
-          </button>
-        </div>
-            <input
-                ref={fileInputRef}
-                type="file"
-                accept=".pdf, image/*, .xlsx, .xls"
-                className="uploadFile-hidden-input"
-                onChange={handleFileChange}
-            />
-        <button className="cancel-button" onClick={onClose}>Cancel</button>
+        {isLoading? (
+          <div className="loader"></div>
+        ) :(
+          <>
+            <h2 className="upload-title">Upload a File</h2>
+            <div className="drag-area">
+              <p>Drag and Drop file here</p>
+              <p>Or</p>
+              <button className="browse-button" onClick={triggerFileInput}>
+                Browse File
+              </button>
+            </div>
+                <input
+                    ref={fileInputRef}
+                    type="file"
+                    accept=".pdf, image/*, .xlsx, .xls"
+                    className="uploadFile-hidden-input"
+                    onChange={handleFileChange}
+                />
+            <button className="cancel-button" onClick={onClose}>Cancel</button>
+          </>
+        )}
+        
       </div>
     </div>
   );
--- frontend/src/LandingPage.jsx
+++ frontend/src/LandingPage.jsx
@@ -1,43 +1,51 @@
 // LandingPage.jsx
-import React, {useState} from 'react'
-import {useNavigate} from 'react-router-dom'
-import UploadModal from './Components/UploadModal';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import UploadModal from "./Components/UploadModal";
+import "./LandingPage.css";
 
-function LandingPage({setUploadedFile}) {
-    const navigate = useNavigate();
-    const [showUploadModal, setShowUploadModal] = useState(false);
+function LandingPage({ setUploadedFile }) {
+  const navigate = useNavigate();
+  const [showUploadModal, setShowUploadModal] = useState(false);
 
-    const handleFileSelect = (file) => {
-        if (file) {
-            setUploadedFile(file);
-            setShowUploadModal(false);
-            // navigate("/dashboard");
-        }
+  const handleFileSelect = (file) => {
+    if (file) {
+      setUploadedFile(file);
+      setShowUploadModal(false);
+      // navigate("/dashboard");
     }
+  };
 
-    return (
-        <>
-            <div>
-                <h1>
-                Welcome to App Name!
-                </h1>
-                <p>Start adding files</p>
-                <button 
-                onClick={() => setShowUploadModal(true)}
-                >
-                    Upload File
-                </button>
-                
-            </div>
-            <UploadModal
-                isOpen={showUploadModal}
-                onClose={() => setShowUploadModal(false)}
-                onFileSelect={handleFileSelect}
-            />
+  return (
+    <>
+      <div className="landing-container">
+        <div className="intro-message">
+            <h1 className="landing-title">
+                Welcome to <span className="app-gradient">App Name!</span>
+            </h1>
+            <p className="landing-subtitle">This is a Procurement Spend Normalizer that extracts invoices, PDFs, spreadsheets, and etc and categorizes it in a smart way for easy readability and convenience.</p>
+            <p className="landing-subtitle">
+                Our app automatically extracts and standardizes spend data from PDFs, scans, and spreadsheets, then makes it instantly searchable with a natural language chatbot.
+            </p>
+            <p className="landing-subtitle">
+            Start adding your files to get results instantly
+            </p>
+        </div>
+        <button
+          onClick={() => setShowUploadModal(true)}
+          className="upload-btn"
+        >
+          Upload File
+        </button>
+      </div>
 
-        </>
-
-    )
+      <UploadModal
+        isOpen={showUploadModal}
+        onClose={() => setShowUploadModal(false)}
+        onFileSelect={handleFileSelect}
+      />
+    </>
+  );
 }
 
 export default LandingPage;
